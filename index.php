b59bfb36f77c3ad07ae5670b2bc90c68c72d4b5a
Anna Heston <amheston@gmail.com>
2016-12-19 13:49:34 -0800

    Added all support pages html, php for mailer, CSS

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>OREdornment | Handmade Jewelry</title>
+    <link rel="stylesheet" href="./ore.css" />
+    <link rel="shortcut icon" href="./images/Ologo.jpg" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+    <script type="text/javascript" src="./ore.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Calligraffitti" rel="stylesheet">
+</head>
+<body class=background>
+<header>
+    <nav class="supportnav">
+        <ul>
+            <a href="./index.html"><li>Home</li></a>
+            <a href="./ourstory.html"><li>Our Story</li></a>
+            <a href="./gallery.html"><li>Gallery</li></a>
+            <a href="./contact.html"><li>Contact</li></a>
+        </ul>
+    </nav>
+</header>
+<section>
+
+</section>
+
+<footer>
+		<a href="https://www.facebook.com/Oredornment"><img src="./images/Logomakr_4amN1P.png" alt="Facebook Logo" class="social-icon"></a>
+		<a href="https://www.instagram.com/oredornment"><img src="./images/Logomakr_7SS89e.png" alt="Instagram Logo" class="social-icon"></a>
+        <p>&copy; 2016 Anna Heston</p>
+</footer>
+</body>

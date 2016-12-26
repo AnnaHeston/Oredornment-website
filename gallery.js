b01c577c51a1fda1729240e2ffb9738a155c822d
Anna Heston <amheston@gmail.com>
2016-12-26 15:13:32 -0800

    simplified JS code and updated DOM code

--- gallery.js
+++ gallery.js
@@ -1,35 +1,29 @@
 
 
 var galleryImages = [
-'ore1', 
-'ore9', 
-'ore11', 
-'ore12'
-];
-
-var galleryNotes = [
-'Turquoise Nugget Earrings',
-'Black Cinnabar Earrings',
-'White Shell Earrings',
-'White Shell and Flower Charm Earrings'
+['ore1', 'Turquoise Nugget Earrings'],
+['ore9', 'Black Cinnabar Earrings'],
+['ore11' 'White Shell Earrings'], 
+['ore12', 'White Shell and Flower Charm Earrings']
 ];
 
 function print(message) {
-	document.write(message);
+	var pictureDiv = document.getElementById ('picture');
+	pictureDiv.innerHTML = message;
 }
 
-function displayImages(list1, list2) {
+function displayImages(list) {
 	var listHTML = '<div class="img">';
 	for (var i = 0; i<list1.length; i +=1) {
-		listHTML += '<img class="img-zoom" src="./images/' + list1[i] + '.png" alt="" width="300" height="200">';
-		listHTML += '<div class="desc">' + list2[i] + '</div>';
+		listHTML += '<img class="img-zoom" src="./images/' + list[i][0] + '.png" alt="" width="300" height="200">';
+		listHTML += '<div class="desc">' + list[i][1] + '</div>';
 				
 	}
 	listHTML += '</div>';
 	print(listHTML);
 }
 
-displayImages (galleryImages, galleryNotes);
+displayImages (galleryImages);
 			  
 			  
 			 
